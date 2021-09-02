2eef6b354f7d890f37811c97b4aab22405de16bb
Khanh Nguyen <nhkhanh2406@gmail.com>
2021-09-02 21:04:15 +0700

    Share project

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+package $package;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Dummy Test
+ */
+public class AppTest {
+
+  public void test() {
+    assertTrue(true);
+  }
+}
